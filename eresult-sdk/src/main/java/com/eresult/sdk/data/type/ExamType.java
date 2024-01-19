2e2043bf9956b033ac9dccbbf0b9bc516e35816d
lazy-pr0grammer <lazyprogrammer21@gmail.com>
2024-01-18 23:47:06 -0500

    feat: new bug fixes and improvements are available

--- eresult-sdk/src/main/java/com/eresult/sdk/data/type/ExamType.java
+++ eresult-sdk/src/main/java/com/eresult/sdk/data/type/ExamType.java
@@ -1,8 +1,11 @@
 package com.eresult.sdk.data.type;
 
 /**
- * Created by Anindya Das on 1/18/24 12:59 PM
- **/
+ * ExamType enum represents different types of exams, such as SSC (Secondary School Certificate) and HSC (Higher Secondary Certificate).
+ * <p>
+ * Created by Anindya Das on 1/18/24 12:59 PM.
+ */
 public enum ExamType {
-    SSC, HSC
+    SSC,  // Secondary School Certificate
+    HSC   // Higher Secondary Certificate
 }
--- eresult-sdk/src/main/java/com/eresult/sdk/data/type/ResultType.java
+++ eresult-sdk/src/main/java/com/eresult/sdk/data/type/ResultType.java
@@ -1,8 +1,14 @@
 package com.eresult.sdk.data.type;
 
 /**
- * Created by Anindya Das on 1/17/24 6:33 AM
- **/
+ * ResultType enum represents different types of result inquiries that can be made through the eboardresults.com SDK.
+ * <p>
+ * Created by Anindya Das on 1/17/24 6:33 AM.
+ */
 public enum ResultType {
-    BOARD, CENTER, DISTRICT, INDIVIDUAL, INSTITUTION
+    BOARD,         // Result for an entire board
+    CENTER,        // Result for a specific center
+    DISTRICT,      // Result for a specific district
+    INDIVIDUAL,    // Individual result
+    INSTITUTION;   // Result for an institution
 }
